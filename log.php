a0967a832b193eb670e0540779402a69f33c4dd9
Jeffrey Schutzman <jeff@pyebrook.com>
2014-11-21 13:59:57 -0500

    added file helpers

--- log.php
+++ log.php
@@ -25,6 +25,55 @@
 if ( !function_exists( 'pbci_log' ) ) {
 
 	/**
+	 * Gets the basename of a plugin.
+	 *
+	 * This method extracts the name of a plugin from its filename.
+	 *
+	 * @uses WP_PLUGIN_DIR, WPMU_PLUGIN_DIR
+	 *
+	 * @param string $file The filename of plugin.
+	 * @return string The name of a plugin.
+	 */
+	function pbci_plugin_basedir( $file ) {
+		global $wp_plugin_paths;
+
+		foreach ( $wp_plugin_paths as $dir => $realdir ) {
+			if ( strpos( $file, $realdir ) === 0 ) {
+				$file = $dir . substr( $file, strlen( $realdir ) );
+			}
+		}
+
+		$file = wp_normalize_path( $file );
+		$plugin_dir = wp_normalize_path( WP_PLUGIN_DIR );
+		$mu_plugin_dir = wp_normalize_path( WPMU_PLUGIN_DIR );
+
+		$base = ( strpos( $file, $plugin_dir ) !== false ) ? $plugin_dir : $mu_plugin_dir;
+
+		$file = preg_replace('#^' . preg_quote($plugin_dir, '#') . '/|^' . preg_quote($mu_plugin_dir, '#') . '/#','',$file); // get relative path from plugins dir
+		$file = trim($file, '/');
+
+		$file = $base . '/' . $file . '/';
+
+		return $file;
+	}
+
+	function pbci_get_calling_file() {
+		$t = debug_backtrace( DEBUG_BACKTRACE_PROVIDE_OBJECT , 2 );
+
+		if ( isset( $t[1]['file'] ) ) {
+			$calling_file = isset( $t[1]['file'] );
+		} else {
+			if ( isset( $t[0]['file'] ) ) {
+				$calling_file = isset( $t[1]['file'] );
+			} else {
+				$calling_file = __FILE__;
+			}
+		}
+
+		return $calling_file;
+	}
+
+	/**
 	 * @param string $text
 	 * @param string $line
 	 * @param string $file
@@ -32,9 +81,10 @@ if ( !function_exists( 'pbci_log' ) ) {
 	 * @param string $class
 	 */
 	function pbci_log( $text = '', $line = '', $file = '', $function = '', $class = '' ) {
-		$do_the_log = ( defined( 'WP_DEBUG' ) && WP_DEBUG );
 
-		$log_file_path = trailingslashit( dirname( __FILE__ ) ) . 'testing.log';
+		$do_the_log = ( defined( 'WP_DEBUG' ) && WP_DEBUG ) || ( defined( 'PBCI_DEBUG' ) && PBCI_DEBUG );
+
+		$log_file_path = trailingslashit( pbci_plugin_basedir( $file ) ) . 'testing.log';
 
 		if ( ! $do_the_log ) {
 			// if logging is turned off, and there is an old log file, make it go away
@@ -44,7 +94,7 @@ if ( !function_exists( 'pbci_log' ) ) {
 		} else {
 			$log_file_path = apply_filters( 'pbci_log', $log_file_path );
 
-			$slug = basename( dirname ( __FILE__ ) );
+			$slug = strtolower( plugin_basename( $file ) );
 
 			$text = html_entity_decode( $text );
 			$msg  =  $slug . ':';
