05cd866252d349f56eecbd82c340a40fbefe3d34
jeff <jeff@pyebrook.com>
2015-02-27 12:07:24 -0500

    check for plugin updates on schedule

--- pbci-plugin.class.php
+++ pbci-plugin.class.php
@@ -85,8 +85,9 @@ if ( ! class_exists( 'pbciPluginV2' ) ) {
 				), 10, 1 );
 
 				$this->get_license_code();
-			}
 
+				add_action( 'wp_update_plugins', array( &$this, 'is_plugin_update_available' ) );
+			}
 		}
 
 		public function get_plugin_name_and_version_filter( $info_array ) {
@@ -547,8 +548,6 @@ if ( ! class_exists( 'pbciPluginV2' ) ) {
 				} else {
 					$remote_version = '';
 				}
-
-				$remote_version = '8.0';
 
 				$current_version = $this->get_plugin_version();
 				if ( version_compare( $current_version, $remote_version, '<' ) ) {
